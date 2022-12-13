b5f287c91859bcb5597b9c144beeaa4b081fb315
Jessica <jess.newman19@gmail.com>
2022-12-13 13:49:18 -0500

    added react-select, removed search capabilites

--- src/CurrencyForm.jsx
+++ src/CurrencyForm.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import Select from "react-select";
 
 function CurrencyForm() {
   const [countries, setCountries] = useState([]);
   const [currencyAmount, setCurrencyAmount] = useState("$1.00");
-  const [query, setQuery] = useState("");
 
   //Populate countries on first load
   useEffect(() => {
@@ -25,20 +25,9 @@ function CurrencyForm() {
     e.preventDefault();
   };
 
-  const filteredCountries = !query
-    ? countries
-    : countries.filter((country) => {
-        if (country.currencies) {
-          return (
-            country.currencies[0].code.includes(query) ||
-            country.currencies[0].name.includes(query)
-          );
-        }
-      });
-
   const mapCountries = () => {
     const uniqueCurrencies = {};
-    filteredCountries.map((country) => {
+    countries.map((country) => {
       if (country.currencies) {
         let code = country.currencies[0].code;
         let name = country.currencies[0].name;
@@ -46,13 +35,12 @@ function CurrencyForm() {
       }
     });
     const objKeys = Object.keys(uniqueCurrencies);
-    return objKeys.map((objKey) => {
-      return (
-        <li key={objKey}>
-          {objKey} - {uniqueCurrencies[objKey]}
-        </li>
-      );
-    });
+    // console.log(objKeys);
+    const options = objKeys.map((objKey) => ({
+      label: `${objKey} - ${uniqueCurrencies[objKey]}`,
+      value: objKey,
+    }));
+    return <Select options={options} />;
   };
 
   return (
@@ -73,44 +61,13 @@ function CurrencyForm() {
 
         <div>
           <label htmlFor="from">From</label>
-          <div>
-            <input
-              id="from"
-              type="text"
-              placeholder="Type to search..."
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-            ></input>
-            <ul role="listbox">{mapCountries()}</ul>
-          </div>
+          <div>{mapCountries()}</div>
         </div>
 
         <div>
           <label htmlFor="from">To</label>
-          <div>
-            <input
-              id="to"
-              type="text"
-              placeholder="Type to search..."
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-            ></input>
-            <ul role="listbox">{mapCountries()}</ul>
-          </div>
+          <div>{mapCountries()}</div>
         </div>
-
-        {/* <label htmlFor="to">To</label>
-        <select id="to" name="to">
-          {countries.map((country) => {
-            if (country.currencies) {
-              return (
-                <option key={country.name}>
-                  {country.currencies[0].code} - {country.currencies[0].name}
-                </option>
-              );
-            }
-          })}
-        </select> */}
         <button>Convert</button>
       </form>
     </div>
